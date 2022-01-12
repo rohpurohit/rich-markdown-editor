0536a7ec8e98ec905feb5d1130342b1538eb45d7
Hadi El-Yakhni <hadiyakhni33@gmail.com>
2022-01-13 00:51:39 +0200

    better exact match

--- src/components/KnowtCommandMenu.tsx
+++ src/components/KnowtCommandMenu.tsx
@@ -593,58 +593,87 @@ class KnowtCommandMenu extends React.Component<Props, State> {
       return this.props.visibleGroups;
     }
 
-    const exactMatchGroup: GroupMenuItem = {
-      groupData: { name: "Exact match" },
-      items: [],
-    };
+    let exactMatchGroupName, exactMatchItemName;
 
     // now that we're searching, filter based on this.props.allGroups,
     // taking into account items with `defaultHidden: true`
-    const filteredGroups = this.props.allGroups.map((group) => {
-      const filteredItems = group.items.filter((item) => {
-        const { name, title, keywords, searchKeyword, customOnClick } = item;
-        if (!this.props.filterable) return true;
+    let filteredGroups = this.props.allGroups
+      .map((group) => {
+        const filteredItems = group.items.filter((item) => {
+          const { name, title, keywords, searchKeyword, customOnClick } = item;
+          if (!this.props.filterable) return true;
 
-        // Some extensions may be disabled, remove corresponding menu items
-        if (
-          name &&
-          !customOnClick &&
-          !this.props.commands[name] &&
-          !this.props.commands[`create${capitalize(name)}`]
-        ) {
-          return false;
-        }
+          // Some extensions may be disabled, remove corresponding menu items
+          if (
+            name &&
+            !customOnClick &&
+            !this.props.commands[name] &&
+            !this.props.commands[`create${capitalize(name)}`]
+          ) {
+            return false;
+          }
 
-        // If no image upload callback has been passed, filter the image block out
-        if (!this.props.uploadImage && name === "image") return false;
+          // If no image upload callback has been passed, filter the image block out
+          if (!this.props.uploadImage && name === "image") return false;
 
-        if (
-          searchKeyword &&
-          searchKeyword?.toLowerCase() === this.props.search?.toLowerCase()
-        ) {
-          exactMatchGroup.items = [item];
-          return false;
-        }
+          if (
+            searchKeyword &&
+            searchKeyword?.toLowerCase() === this.props.search?.toLowerCase()
+          ) {
+            // found an exact match, save it to be used later
+            exactMatchGroupName = group.groupData.name;
+            exactMatchItemName = item.title;
 
-        return [
-          group.groupData.name,
-          title,
-          keywords,
-          searchKeyword,
-        ].some((str) =>
-          str?.toLowerCase().includes(this.props.search?.toLowerCase())
-        );
-      });
+            return true;
+          }
 
-      return {
-        ...group,
-        items: filteredItems,
-      };
-    });
+          return [
+            group.groupData.name,
+            title,
+            keywords,
+            searchKeyword,
+          ].some((str) =>
+            str?.toLowerCase().includes(this.props.search?.toLowerCase())
+          );
+        });
 
-    return [exactMatchGroup, ...filteredGroups].filter(
-      ({ items }) => items.length
+        return {
+          ...group,
+          items: filteredItems,
+        };
+      })
+      .filter(({ items }) => items.length);
+
+    // if no exact match, just return the matched groups
+    if (!exactMatchGroupName) return filteredGroups;
+
+    // move the exact matched group to the top
+    filteredGroups = this.moveArrayItemToTop(
+      filteredGroups,
+      filteredGroups.findIndex(
+        (group) => group.groupData.name === exactMatchGroupName
+      )
     );
+
+    // move the exact match item to top of the exact match group
+    filteredGroups[0].items = this.moveArrayItemToTop(
+      filteredGroups[0].items,
+      filteredGroups[0].items.findIndex(
+        (item) => item.title === exactMatchItemName
+      )
+    );
+
+    return filteredGroups;
+  }
+
+  moveArrayItemToTop<T>(array: T[], index: number): T[] {
+    if (index === -1) return array;
+
+    const newArray = [...array];
+    newArray.splice(index, 1);
+    newArray.unshift(array[index]);
+
+    return newArray;
   }
 
   renderGroups(): React.ReactNode {
