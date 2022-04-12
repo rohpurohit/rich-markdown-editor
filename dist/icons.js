6f7fcf114f6dc95ec5acfc6e7525098ec0e01ca3
Hadi El-Yakhni <hadiyakhni33@gmail.com>
2022-04-12 23:46:51 +0200

    build

--- dist/icons.js
+++ dist/icons.js
@@ -23,14 +23,14 @@ const CircleIcon = (_a) => {
         react_1.default.createElement("circle", Object.assign({}, rest))));
 };
 exports.CircleIcon = CircleIcon;
-const RemoveIcon = ({ color, size, }) => (react_1.default.createElement(outline_icons_1.Icon, { color: color, size: size },
-    react_1.default.createElement("svg", { height: "20px", version: "1.1", viewBox: "0 0 20 20", width: "20px" },
-        react_1.default.createElement("title", null),
-        react_1.default.createElement("desc", null),
-        react_1.default.createElement("defs", null),
-        react_1.default.createElement("g", { fill: "none", fillRule: "evenodd", id: "Page-1", stroke: "none", strokeWidth: "1" },
-            react_1.default.createElement("g", { fill: color, id: "Core", transform: "translate(-2.000000, -380.000000)" },
-                react_1.default.createElement("g", { id: "remove-circle-outline", transform: "translate(2.000000, 380.000000)" },
-                    react_1.default.createElement("path", { d: "M5,9 L5,11 L15,11 L15,9 L5,9 L5,9 Z M10,0 C4.5,0 0,4.5 0,10 C0,15.5 4.5,20 10,20 C15.5,20 20,15.5 20,10 C20,4.5 15.5,0 10,0 L10,0 Z M10,18 C5.6,18 2,14.4 2,10 C2,5.6 5.6,2 10,2 C14.4,2 18,5.6 18,10 C18,14.4 14.4,18 10,18 L10,18 Z", id: "Shape" })))))));
+const RemoveIcon = ({ color, size, }) => (react_1.default.createElement("div", { style: { marginTop: 4, marginLeft: 4 } },
+    react_1.default.createElement(outline_icons_1.Icon, { color: color, size: size },
+        react_1.default.createElement("svg", { width: "19px", height: "16px", viewBox: "0 0 19 16", version: "1.1", xmlns: "http://www.w3.org/2000/svg" },
+            react_1.default.createElement("title", null, "icon"),
+            react_1.default.createElement("g", { id: "Knowt-6-Editor", stroke: "none", "stroke-width": "1", fill: "none", "fill-rule": "evenodd" },
+                react_1.default.createElement("g", { id: "Artboard", transform: "translate(-70.000000, -56.000000)", fill: "#FFFFFF" },
+                    react_1.default.createElement("g", { id: "icon", transform: "translate(70.000000, 56.000000)" },
+                        react_1.default.createElement("path", { d: "M15,0 C15.5128358,0 15.9355072,0.38604019 15.9932723,0.88337888 L16,1 L16,5.61483519 C16,5.69690861 15.9966323,5.77881993 15.9899356,5.8603674 C15.9966912,5.90581613 16,5.95252166 16,6 C16,6.1805703 15.9521405,6.3499624 15.8684145,6.4961832 C15.8658881,6.5018105 15.864041,6.5077569 15.8621752,6.513698 L15.7854301,6.7290072 L14,11.191 L14,15 C14,15.5128358 13.6139598,15.9355072 13.1166211,15.9932723 L13,16 L1,16 C0.44771525,16 0,15.5522847 0,15 C0,14.4871642 0.38604019,14.0644928 0.88337888,14.0067277 L1,14 L8,14 L8,11.194 L6.2145699,6.7290072 C6.1839016,6.6523364 6.1564596,6.5745518 6.1322835,6.495859 C6.0478595,6.3499624 6,6.1805703 6,6 C6,5.95252166 6.0033088,5.90581613 6.0097083,5.86010142 C6.0095959,5.85466244 6.0091437,5.84895569 6.0087078,5.84324723 L6,5.61483519 L6,1 C6,0.44771525 6.4477152,0 7,0 C7.5128358,0 7.9355072,0.38604019 7.9932723,0.88337888 L8,1 L8,5 L14,5 L14,1 C14,0.44771525 14.4477153,0 15,0 Z M12,12 L10,12 L10,14 L12,14 L12,12 Z M13.522,7 L8.477,7 L9.677,10 L12.322,10 L13.522,7 Z", id: "Shape", "fill-rule": "nonzero" }),
+                        react_1.default.createElement("path", { d: "M-0.593312439,6.65971037 L18.8153832,6.65971037 C19.2187912,6.65971037 19.545818,6.98673716 19.545818,7.39014515 C19.545818,7.79355314 19.2187912,8.12057994 18.8153832,8.12057994 L-0.593312439,8.12057994 C-0.99672043,8.12057994 -1.32374722,7.79355314 -1.32374722,7.39014515 C-1.32374722,6.98673716 -0.99672043,6.65971037 -0.593312439,6.65971037 Z", id: "Rectangle", transform: "translate(9.111035, 7.390145) rotate(35.000000) translate(-9.111035, -7.390145) " }))))))));
 exports.RemoveIcon = RemoveIcon;
 //# sourceMappingURL=icons.js.map
