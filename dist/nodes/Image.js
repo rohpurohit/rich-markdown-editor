16e27923e5e0ddb45eaa4dafb6d39e806d4ca7bd
Hadi El-Yakhni <hadiyakhni33@gmail.com>
2022-05-08 15:25:49 +0200

    build

--- dist/nodes/Image.js
+++ dist/nodes/Image.js
@@ -33,6 +33,8 @@ const Node_1 = __importDefault(require("./Node"));
 const useResizeObserver_1 = __importDefault(require("../hooks/useResizeObserver"));
 const imsize_1 = __importDefault(require("../rules/imsize"));
 const IMAGE_INPUT_REGEX = /!\[(?<alt>[^\]\[]*?)]\((?<filename>[^\]\[]*?)(?=\“|\))\“?(?<layoutclass>[^\]\[\”]+)?\”?\)$/;
+const resizeIcon = (React.createElement("svg", { viewBox: "0 0 512 512", style: { transform: "rotate(-90deg)" } },
+    React.createElement("path", { fill: "#fff", d: "M208 281.4c-12.5-12.5-32.76-12.5-45.26-.002l-78.06 78.07l-30.06-30.06c-6.125-6.125-14.31-9.367-22.63-9.367c-4.125 0-8.279 .7891-12.25 2.43c-11.97 4.953-19.75 16.62-19.75 29.56v135.1C.0013 501.3 10.75 512 24 512h136c12.94 0 24.63-7.797 29.56-19.75c4.969-11.97 2.219-25.72-6.938-34.87l-30.06-30.06l78.06-78.07c12.5-12.49 12.5-32.75 .002-45.25L208 281.4zM487.1 0h-136c-12.94 0-24.63 7.797-29.56 19.75c-4.969 11.97-2.219 25.72 6.938 34.87l30.06 30.06l-78.06 78.07c-12.5 12.5-12.5 32.76 0 45.26l22.62 22.62c12.5 12.5 32.76 12.5 45.26 0l78.06-78.07l30.06 30.06c9.156 9.141 22.87 11.84 34.87 6.937C504.2 184.6 512 172.9 512 159.1V23.1C512 10.74 501.3 0 487.1 0z" })));
 const uploadPlugin = (options) => {
     return new prosemirror_state_1.Plugin({
         props: {
@@ -154,7 +156,9 @@ class Image extends Node_1.default {
             return (React.createElement("div", { contentEditable: false, className: className },
                 React.createElement(ImageWrapper, { className: isSelected ? "ProseMirror-selectednode" : "", onClick: this.handleSelect(props) },
                     React.createElement(ResizableWrapper, Object.assign({ ref: resizableWrapperRef }, { width, height }),
-                        React.createElement("img", { src: src, alt: alt, title: title }))),
+                        React.createElement("img", { src: src, alt: alt, title: title }),
+                        React.createElement(ResizeButtonConatiner, null,
+                            React.createElement(ResizeIconContainer, null, resizeIcon)))),
                 React.createElement(Caption, { onKeyDown: this.handleCaptionKeyDown(props), onBlur: this.handleCaptionBlur(props), className: "caption", tabIndex: -1, role: "textbox", contentEditable: true, suppressContentEditableWarning: true, "data-caption": this.options.dictionary.imageCaptionPlaceholder }, alt)));
         };
     }
@@ -296,6 +300,11 @@ const ResizableWrapper = styled_components_1.default.div `
   resize: both;
   overflow: hidden;
   max-height: 75%;
+  position: relative;
+
+  &::-webkit-resizer {
+    display: none;
+  }
 
   ${({ width, height }) => width &&
     height &&
@@ -371,5 +380,24 @@ const ImageWrapper = styled_components_1.default.span `
   &.ProseMirror-selectednode + ${Caption} {
     visibility: visible;
   }
+`;
+const ResizeButtonConatiner = styled_components_1.default.div `
+  min-width: 22px;
+  min-height: 22px;
+  max-width: 22px;
+  max-height: 22px;
+  background-color: black;
+  position: absolute;
+  bottom: -5px;
+  right: -5px;
+  border-radius: 50%;
+`;
+const ResizeIconContainer = styled_components_1.default.div `
+  min-width: 10px;
+  min-height: 10px;
+  max-width: 10px;
+  max-height: 10px;
+  margin-top: 5px;
+  margin-left: 5px;
 `;
 //# sourceMappingURL=Image.js.map
